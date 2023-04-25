f2045be08c3800249abe19a75e6f2979eff9a6b1
Hajogin <xaraim@inbox.ru>
2023-04-25 17:22:27 +0500

    знакомство с переменными и типом данных в РНР

---
+++ PHP/2/3.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8">
+<?php
+$a = 10;
+$b = 5;
+$result = $a + $b;
+echo $result;
+//var_dump($result);
+
+?>
---
+++ PHP/2/5.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<?php
+//$a = null;
+//echo $a;
+//var_dump($a);
+$a = 10;
+$b = '10';
+echo $a + $b;
+
+?>
